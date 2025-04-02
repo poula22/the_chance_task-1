3d686c52a035d76fbbc6e3da4e056c30b34d9f04
pola george <86931053+poula22@users.noreply.github.com>
2025-04-03 00:05:54 +0200

    Sudoku performance enhancement: - add are areSudokuNumbersValid function - optimise areSudokuGridsValid performance - change Sudoku Flow Chart

--- src/problem/Sudoku.kt
+++ src/problem/Sudoku.kt
@@ -16,25 +16,23 @@ fun isValidSudoku(board: List<List<String>>): Boolean {
     val divisionFactor = sqrt(columnSize.toDouble())
     if ((divisionFactor - divisionFactor.toInt()) != 0.0) return false
 
-    if (!areSudokuColumnsValid(board, rowSize, columnSize)) return false
-    if (!areSudokuRowsValid(board, rowSize, columnSize)) return false
+    if (!areSudokuNumbersValid(board, columnSize, divisionFactor.toInt())) return false
+    if (!areSudokuColumnsValid(board, columnSize)) return false
+    if (!areSudokuRowsValid(board, columnSize)) return false
 
     return areSudokuGridsValid(board, divisionFactor.toInt())
 }
 
-
 /**
  * Time complexity: O(n2)
-* */
-private fun areSudokuRowsValid(board: List<List<String>>, rowSize: Int, columnSize: Int): Boolean {
-    for (row in 0 until rowSize) {
+ * */
+private fun areSudokuRowsValid(board: List<List<String>>, size: Int): Boolean {
+    for (row in 0..<size) {
         val numbersSet = mutableSetOf<String>()
-        var dashCounter = 0
-        for (column in 0 until columnSize) {
-            if (board[row][column] == "-") dashCounter++
-            else numbersSet.add(board[row][column])
+        for (column in 0..<size) {
+            if (board[row][column] == "-") continue
+            if (!numbersSet.add(board[row][column])) return false
         }
-        if ((numbersSet.size + dashCounter) != rowSize) return false
     }
     return true
 }
@@ -42,44 +40,43 @@ private fun areSudokuRowsValid(board: List<List<String>>, rowSize: Int, columnSi
 /**
  * Time complexity: O(n2)
  * */
-private fun areSudokuColumnsValid(board: List<List<String>>, rowSize: Int, columnSize: Int): Boolean {
-    for (column in 0 until columnSize) {
+private fun areSudokuColumnsValid(board: List<List<String>>, size: Int): Boolean {
+    for (column in 0..<size) {
         val numbersSet = mutableSetOf<String>()
-        var dashCounter = 0
-        for (row in 0 until rowSize) {
-            if (board[row][column] == "-") dashCounter++
-            else numbersSet.add(board[row][column])
+        for (row in 0..<size) {
+            if (board[row][column] == "-") continue
+            if (!numbersSet.add(board[row][column])) return false
         }
-        if ((numbersSet.size + dashCounter) != columnSize) return false
     }
     return true
 }
 
-/**
- * based on binary we have four grids each one will be labeled starting from top left with 00,01,10,11.
- * The first digit will help us to calculate row start index. (digit * divisionFactor)
- * The second digit will help us to calculate column start index. (digit * divisionFactor)
- * Time complexity: O(n2) - we will neglect constant of 4 in (4 * n2)
-* */
 private fun areSudokuGridsValid(board: List<List<String>>, divisionFactor: Int): Boolean {
-    for (grid in 0..< 4) {
-        val rowStartIndex = (grid / 2) * divisionFactor
-        val colStartIndex = (grid % 2) * divisionFactor
-        val rowLastIndex = rowStartIndex + divisionFactor
-        val colLastIndex = colStartIndex + divisionFactor
-        val list = MutableList(divisionFactor * divisionFactor) { (it + 1).toString() }
-
-        for (row in rowStartIndex ..< rowLastIndex) {
-            for (col in colStartIndex ..< colLastIndex) {
-                val element = board[row][col]
-                if (element != "-") {
-                    if (element in list) {
-                        list.remove(element)
-                    } else {
-                        return false
-                    }
+    for (boxRow in 0..<divisionFactor) {
+        for (boxCol in 0..<divisionFactor) {
+            val numbersSet = mutableSetOf<String>()
+            for (row in 0..<divisionFactor) {
+                for (col in 0..<divisionFactor) {
+                    val element = board[boxRow * divisionFactor + row][boxCol * divisionFactor + col]
+                    if (element == "-") continue
+                    if (!numbersSet.add(element)) return false
                 }
             }
+        }
+    }
+    return true
+}
+
+private fun areSudokuNumbersValid(
+    board: List<List<String>>,
+    size: Int,
+    divisionFactor: Int
+): Boolean {
+    for (row in 0..<size) {
+        for (column in 0..<size) {
+            if (board[row][column] == "-") continue
+            val element = board[row][column].toIntOrNull() ?: return false
+            if (element < 1 || element > (divisionFactor * divisionFactor)) return false
         }
     }
     return true
