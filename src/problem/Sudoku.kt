70e3c0acde811956ac3661f5b789dfc0edaf0b61
pola george <86931053+poula22@users.noreply.github.com>
2025-04-02 23:05:50 +0200

    Sudoku performance enhancement: - Change rows and columns checks strategy to use HashSet to optimize performance to O(n2). - Change grids check way to iterate through grid directly to optimize performance to O(n2)

--- src/problem/Sudoku.kt
+++ src/problem/Sudoku.kt
@@ -2,6 +2,9 @@ package problem
 
 import kotlin.math.sqrt
 
+/**
+ * Time complexity: O(n2)
+ * */
 fun isValidSudoku(board: List<List<String>>): Boolean {
     if (board.isEmpty()) return false
     val columnSize = board[0].size
@@ -11,90 +14,64 @@ fun isValidSudoku(board: List<List<String>>): Boolean {
     if (columnSize < 4) return false
 
     val divisionFactor = sqrt(columnSize.toDouble())
-    if ((divisionFactor - divisionFactor.toInt()) !=  0.0) return false
+    if ((divisionFactor - divisionFactor.toInt()) != 0.0) return false
 
     if (!areSudokuColumnsValid(board, rowSize, columnSize)) return false
     if (!areSudokuRowsValid(board, rowSize, columnSize)) return false
 
-    val sudokuGrids = initSudokuGrids(board, rowSize, columnSize, divisionFactor.toInt())
-    return isSudokuGridsValid(sudokuGrids)
-
+    return areSudokuGridsValid(board, divisionFactor.toInt())
 }
 
-private fun initSudokuGrids(
-    board: List<List<String>>,
-    rowSize: Int,
-    columnSize: Int,
-    divisionFactor: Int
-): List<List<List<String>>> {
-    val grids: MutableList<MutableList<MutableList<String>>> = mutableListOf()
-    val nOfGrids = (rowSize / divisionFactor) * (columnSize / divisionFactor)
 
-    for (gridIndex in 0 until nOfGrids) {
-        val grid = mutableListOf<MutableList<String>>()
-        for (row in 0 until divisionFactor) {
-            val list = mutableListOf<String>()
-            for (column in 0 until divisionFactor) {
-                list.add("-")
-            }
-            grid.add(list)
-        }
-        grids.add(grid)
-    }
-
-    var startRowIndex = 0
-    var startColumnIndex = -divisionFactor
-
-    for (gridIndex in 0 until nOfGrids) {
-        startColumnIndex += divisionFactor
-        if (startColumnIndex > columnSize - 1) {
-            startColumnIndex = 0
-            startRowIndex += divisionFactor
-        }
-        for (gridRow in 0 until divisionFactor) {
-            for (gridColumn in 0 until divisionFactor) {
-                grids[gridIndex][gridRow][gridColumn] = board[startRowIndex + gridRow][startColumnIndex + gridColumn]
-            }
-        }
-    }
-    return grids
-}
-
-//o(n2)
+/**
+ * Time complexity: O(n2)
+* */
 private fun areSudokuRowsValid(board: List<List<String>>, rowSize: Int, columnSize: Int): Boolean {
-    val sortedBoard = board.map { it.sorted() }
-
     for (row in 0 until rowSize) {
-        for (column in 0 until columnSize - 1) {
-            if (sortedBoard[row][column] == "-") continue
-            if (sortedBoard[row][column] == sortedBoard[row][column + 1]) return false
+        val numbersSet = mutableSetOf<String>()
+        var dashCounter = 0
+        for (column in 0 until columnSize) {
+            if (board[row][column] == "-") dashCounter++
+            else numbersSet.add(board[row][column])
         }
+        if ((numbersSet.size + dashCounter) != rowSize) return false
     }
     return true
 }
 
+/**
+ * Time complexity: O(n2)
+ * */
 private fun areSudokuColumnsValid(board: List<List<String>>, rowSize: Int, columnSize: Int): Boolean {
-    for (colum in 0 until columnSize) {
-        for (row in 0 until rowSize - 1) {
-            if (board[row][colum] == "-") continue
-
-            for (nextRow in (row + 1) until rowSize) {
-                if (board[row][colum] == board[nextRow][colum]) return false
-            }
+    for (column in 0 until columnSize) {
+        val numbersSet = mutableSetOf<String>()
+        var dashCounter = 0
+        for (row in 0 until rowSize) {
+            if (board[row][column] == "-") dashCounter++
+            else numbersSet.add(board[row][column])
         }
+        if ((numbersSet.size + dashCounter) != columnSize) return false
     }
     return true
 }
 
-private fun isSudokuGridsValid(sudokuGrids: List<List<List<String>>>): Boolean {
-    if (sudokuGrids.isEmpty()) return false
-    val gridSize = sudokuGrids[0].size
+/**
+ * based on binary we have four grids each one will be labeled starting from top left with 00,01,10,11.
+ * The first digit will help us to calculate row start index. (digit * divisionFactor)
+ * The second digit will help us to calculate column start index. (digit * divisionFactor)
+ * Time complexity: O(n2) - we will neglect constant of 4 in (4 * n2)
+* */
+private fun areSudokuGridsValid(board: List<List<String>>, divisionFactor: Int): Boolean {
+    for (grid in 0..< 4) {
+        val rowStartIndex = (grid / 2) * divisionFactor
+        val colStartIndex = (grid % 2) * divisionFactor
+        val rowLastIndex = rowStartIndex + divisionFactor
+        val colLastIndex = colStartIndex + divisionFactor
+        val list = MutableList(divisionFactor * divisionFactor) { (it + 1).toString() }
 
-    for (grid in sudokuGrids) {
-        val list = MutableList(gridSize * gridSize) { (it+1).toString() }
-
-        for (row in grid) {
-            for (element in row) {
+        for (row in rowStartIndex ..< rowLastIndex) {
+            for (col in colStartIndex ..< colLastIndex) {
+                val element = board[row][col]
                 if (element != "-") {
                     if (element in list) {
                         list.remove(element)
